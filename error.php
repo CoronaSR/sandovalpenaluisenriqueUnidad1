dff09c640d30e4bf9e114b815077b114778ca3e0
ItsArtiik <126124046+ItsArtiik@users.noreply.github.com>
2023-05-27 21:10:23 -0600

    Vista del error
    se agregó vista de error

---
+++ error.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error!</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" type="text/css" rel="stylesheet">
+
+
+<body class="bg-main">
+<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center row">
+                <div class=" col-md-4">
+                    <img src="img/error.png" alt=""
+                        class="img-fluid">
+                </div>
+                <div class=" col-md-5 mt-3">
+                    <p class="fs-3"> <span class="text-danger">Oops!</span> Se ha encontrado un error.</p>
+                    <p class="lead">
+                        Contacte a soporte.
+                    </p>
+
+                    <button type="button" onclick='window.location.replace("../SHGAB/login.php")' class="button-main">Regresar al inicio.</button>
+                </div>
+
+            </div>
+        </div>
+</body>
+</html>
